a761081ca4ff3f65b347863c3cbabb3865e3c766
Theflashofkills <vinicius.araujo.44@hotmail.com>
2023-10-19 20:26:56 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,32 @@
+package AC2;
+
+class Produto {
+    private int codigo;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private java.time.LocalDate dataValidade;
+
+    public Produto(int codigo, String nome, String descricao, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public void setDataValidade(java.time.LocalDate dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+
+    public boolean estaVencido() {
+        if (dataValidade != null) {
+            return java.time.LocalDate.now().isAfter(dataValidade);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Código do Produto: " + codigo + "\nNome: " + nome + "\nDescrição: " + descricao + "\nPreço: R$" + preco;
+    }
+}
