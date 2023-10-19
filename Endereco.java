be1dc77bddccf2d181ad79ca84b844a3d4d50678
Theflashofkills <vinicius.araujo.44@hotmail.com>
2023-10-19 20:26:51 -0300

    Create Endereco.java

---
+++ Endereco.java
@@ -0,0 +1,24 @@
+package AC2;
+
+class Endereco {
+    private String rua;
+    private int numero;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String estado;
+
+    public Endereco(String rua, int numero, String bairro, String cep, String cidade, String estado) {
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return rua + ", " + numero + ", " + bairro + ", " + cep + ", " + cidade + ", " + estado;
+    }
+}
