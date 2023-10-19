2d810cfa03cc7ba636f566bd19c52d37f4fe1819
Theflashofkills <vinicius.araujo.44@hotmail.com>
2023-10-19 20:26:44 -0300

    Create ClientePJ.java

---
+++ ClientePJ.java
@@ -0,0 +1,19 @@
+package AC2;
+
+class ClientePJ extends Cliente {
+    private String cnpj;
+    private String razaoSocial;
+    private int prazoMaximo;
+
+    public ClientePJ(String nome, Endereco endereco, String cnpj, String razaoSocial, int prazoMaximo) {
+        super(nome, endereco);
+        this.cnpj = cnpj;
+        this.razaoSocial = razaoSocial;
+        this.prazoMaximo = prazoMaximo;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nCNPJ: " + cnpj + "\nRazão Social: " + razaoSocial + "\nPrazo Máximo: " + prazoMaximo + " dias";
+    }
+}
