18236d65f0af8ae21db0695efdb965311ea287bb
Theflashofkills <vinicius.araujo.44@hotmail.com>
2023-11-06 18:55:55 -0300

    Update SistemaDeCompras.java

--- SistemaDeCompras.java
+++ SistemaDeCompras.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -67,6 +69,16 @@ public class SistemaDeCompras {
         } while (opcao != 8);
     }
 
+
+
+
+
+
+
+
+
+
+
     public void cadastrarCliente() {
         Scanner scanner = new Scanner(System.in);
     
@@ -78,7 +90,7 @@ public class SistemaDeCompras {
             String rua = scanner.nextLine();
             System.out.print("Endereço - Número: ");
             int numero = scanner.nextInt();
-            scanner.nextLine(); // Consume newline
+            scanner.nextLine();
             System.out.print("Endereço - Bairro: ");
             String bairro = scanner.nextLine();
             System.out.print("Endereço - CEP: ");
@@ -151,6 +163,13 @@ public class SistemaDeCompras {
     
     
 
+
+
+
+
+
+
+
     public void deletarClientePorCpfOuCnpj() {
         Scanner scanner = new Scanner(System.in);
     
@@ -210,6 +229,11 @@ public class SistemaDeCompras {
 
    
     
+
+
+
+
+
     
     public void deletarClientePorNome() {
         Scanner scanner = new Scanner(System.in);
@@ -242,7 +266,7 @@ public class SistemaDeCompras {
                 System.out.println("2 - Inserir nome novamente");
                 System.out.print("Escolha uma opção: ");
                 opcao = scanner.nextInt();
-                scanner.nextLine(); // Consume newline
+                scanner.nextLine();
                 if (opcao == 1) {
                     return;
                 } else if (opcao != 2) {
@@ -330,6 +354,16 @@ public class SistemaDeCompras {
             }
     }
 
+
+
+
+
+
+
+
+
+
+
     public void efetuarCompra() {
         Scanner scanner = new Scanner(System.in);
     
@@ -412,12 +446,373 @@ public class SistemaDeCompras {
 
 
 
+
+
+
+
+
+
+
+
+
     public void atualizarPagamentoCompra() {
-        // Implemente a lógica para atualizar o pagamento de uma compra.
+        Scanner scanner = new Scanner(System.in);
+    
+        System.out.println("\nAtualizar Pagamento de Compra:");
+        System.out.println("Opções:");
+        System.out.println("1 - Procurar compra por CPF");
+        System.out.println("2 - Procurar por Identificador da Compra");
+        System.out.println("3 - Sair");
+        System.out.print("Escolha uma opção: ");
+        int opcao = scanner.nextInt();
+        scanner.nextLine();
+    
+        switch (opcao) {
+            case 1:
+                System.out.print("Digite o CPF do cliente: ");
+                String cpf = scanner.nextLine();
+    
+                List<Compra> comprasCliente = new ArrayList<>();
+    
+                // Encontrar todas as compras do cliente com o CPF fornecido
+                for (Compra compra : compras) {
+                    if (compra.getCpfOuCnpjCliente().equals(cpf)) {
+                        comprasCliente.add(compra);
+                    }
+                }
+    
+                if (comprasCliente.isEmpty()) {
+                    System.out.println("Nenhuma compra encontrada para este CPF.");
+                    break;
+                }
+    
+                System.out.println("Compras do cliente com CPF " + cpf + ":");
+                for (Compra compra : comprasCliente) {
+                    System.out.println(compra);
+                }
+    
+                System.out.print("Escolha o número da compra que deseja atualizar (ou 0 para cancelar): ");
+                int escolhaCompra = scanner.nextInt();
+                scanner.nextLine();
+    
+                if (escolhaCompra == 0 || escolhaCompra > comprasCliente.size()) {
+                    System.out.println("Operação cancelada. Voltando ao menu principal.");
+                    break;
+                }
+    
+                Compra compraEscolhida = comprasCliente.get(escolhaCompra - 1);
+    
+                System.out.print("Digite o novo total pago pelo cliente: R$");
+                double novoTotalPago = scanner.nextDouble();
+                scanner.nextLine();
+    
+                double totalRestante = compraEscolhida.calcularValorRestante();
+                
+                if (novoTotalPago > totalRestante) {
+                    System.out.println("Novo total excederá o valor total da compra. Insira novamente.");
+                } else {
+                    compraEscolhida.atualizarTotalPago(novoTotalPago);
+                    System.out.println("Total pago atualizado com sucesso.");
+                }
+    
+                break;
+    
+            case 2:
+                System.out.print("Digite o Identificador da Compra: ");
+                int identificadorCompra = scanner.nextInt();
+                scanner.nextLine();
+    
+                Compra compraEncontrada = null;
+    
+                for (Compra compra : compras) {
+                    if (compra.getIdentificador() == identificadorCompra) {
+                        compraEncontrada = compra;
+                        break;
+                    }
+                }
+    
+                if (compraEncontrada == null) {
+                    System.out.println("Compra com Identificador " + identificadorCompra + " não encontrada.");
+                    break;
+                }
+    
+                System.out.println("Compra encontrada:");
+                System.out.println(compraEncontrada);
+    
+                System.out.print("Digite o novo total pago pelo cliente: R$");
+                novoTotalPago = scanner.nextDouble();
+                scanner.nextLine();
+    
+                double totalRestanteCompra = compraEncontrada.calcularValorRestante();
+    
+                if (novoTotalPago > totalRestanteCompra) {
+                    System.out.println("Novo total excederá o valor total da compra. Insira novamente.");
+                } else {
+                    compraEncontrada.atualizarTotalPago(novoTotalPago);
+                    System.out.println("Total pago atualizado com sucesso.");
+                }
+    
+                break;
+    
+            case 3:
+                System.out.println("Operação cancelada. Voltando ao menu principal.");
+                break;
+    
+            default:
+                System.out.println("Opção inválida. Tente novamente.");
+                break;
+        }
     }
+    
+    
+
+
+
+
+
+
 
     public void exibirRelatorios() {
-        // Implemente a lógica para exibir os relatórios solicitados.
+    Scanner scanner = new Scanner(System.in);
+
+    int opcao;
+
+    do {
+        System.out.println("\nRelatórios:");
+        System.out.println("1 - Relação de Clientes por Nome");
+        System.out.println("2 - Relação de Todos os Produtos");
+        System.out.println("3 - Busca de Produto pelo Nome");
+        System.out.println("4 - Relação de Todas as Compras");
+        System.out.println("5 - Busca de Compra por Número");
+        System.out.println("6 - Compras Não Pagas");
+        System.out.println("7 - Últimas 10 Compras Pagas");
+        System.out.println("8 - Compra Mais Cara");
+        System.out.println("9 - Compra Mais Barata");
+        System.out.println("10 - Valor Total de Compras nos Últimos 12 Meses");
+        System.out.println("11 - Voltar ao Menu Principal");
+
+        System.out.print("Escolha uma opção: ");
+        opcao = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (opcao) {
+            case 1:
+                System.out.print("Digite a sequência de caracteres iniciais do nome: ");
+                String sequencia = scanner.nextLine();
+                relacaoClientesPorNome(sequencia);
+                break;
+            case 2:
+                relacaoTodosProdutos();
+                break;
+            case 3:
+                System.out.print("Digite o nome do produto: ");
+                String nomeProduto = scanner.nextLine();
+                buscaProdutoPeloNome(nomeProduto);
+                break;
+            case 4:
+                relacaoTodasCompras();
+                break;
+            case 5:
+                System.out.print("Digite o número da compra: ");
+                int numeroCompra = scanner.nextInt();
+                buscaCompraPeloNumero(numeroCompra);
+                break;
+            case 6:
+                comprasNaoPagas();
+                break;
+            case 7:
+                ultimas10ComprasPagas();
+                break;
+            case 8:
+                compraMaisCara();
+                break;
+            case 9:
+                compraMaisBarata();
+                break;
+            case 10:
+                valorTotalComprasUltimos12Meses();
+                break;
+            case 11:
+                return;
+            default:
+                System.out.println("Opção inválida. Tente novamente.");
+        }
+    } while (opcao != 11);
+}
+
+private void relacaoClientesPorNome(String sequencia) {
+    List<Cliente> clientesEncontrados = new ArrayList<>();
+
+    for (Cliente cliente : clientes) {
+        if (cliente.getNome().toLowerCase().startsWith(sequencia.toLowerCase())) {
+            clientesEncontrados.add(cliente);
+        }
     }
+
+    if (clientesEncontrados.isEmpty()) {
+        System.out.println("Histórico não encontrado.");
+    } else {
+        System.out.println("Clientes com nome iniciado por \"" + sequencia + "\":");
+        for (Cliente cliente : clientesEncontrados) {
+            System.out.println(cliente);
+        }
+    }
+}
+
+private void relacaoTodosProdutos() {
+    if (produtos.isEmpty()) {
+        System.out.println("Histórico não encontrado.");
+    } else {
+        System.out.println("Todos os Produtos:");
+        for (Produto produto : produtos) {
+            System.out.println(produto);
+        }
+    }
+}
+
+private void buscaProdutoPeloNome(String nomeProduto) {
+    boolean encontrado = false;
+
+    for (Produto produto : produtos) {
+        if (produto.getNome().equalsIgnoreCase(nomeProduto)) {
+            encontrado = true;
+            System.out.println("Produto Encontrado:");
+            System.out.println(produto);
+            break;
+        }
+    }
+
+    if (!encontrado) {
+        System.out.println("Histórico não encontrado.");
+    }
+}
+
+private void relacaoTodasCompras() {
+    if (compras.isEmpty()) {
+        System.out.println("Histórico não encontrado.");
+    } else {
+        System.out.println("Todas as Compras:");
+        for (Compra compra : compras) {
+            System.out.println(compra);
+        }
+    }
+}
+
+private void buscaCompraPeloNumero(int numeroCompra) {
+    boolean encontrado = false;
+
+    for (Compra compra : compras) {
+        if (compra.getIdentificador() == numeroCompra) {
+            encontrado = true;
+            System.out.println("Compra Encontrada:");
+            System.out.println(compra);
+            break;
+        }
+    }
+
+    if (!encontrado) {
+        System.out.println("Histórico não encontrado.");
+    }
+}
+
+private void comprasNaoPagas() {
+    boolean encontradas = false;
+
+    for (Compra compra : compras) {
+        if (compra.calcularValorRestante() > 0) {
+            encontradas = true;
+            System.out.println("Compra Não Paga:");
+            System.out.println(compra);
+        }
+    }
+
+    if (!encontradas) {
+        System.out.println("Histórico não encontrado.");
+    }
+}
+
+private void ultimas10ComprasPagas() {
+    List<Compra> ultimasComprasPagas = new ArrayList<>();
+
+    for (int i = compras.size() - 1; i >= 0; i--) {
+        Compra compra = compras.get(i);
+        if (compra.getTotalPago() == compra.getValorTotal()) {
+            ultimasComprasPagas.add(compra);
+        }
+        if (ultimasComprasPagas.size() == 10) {
+            break;
+        }
+    }
+
+    if (ultimasComprasPagas.isEmpty()) {
+        System.out.println("Histórico não encontrado.");
+    } else {
+        System.out.println("Últimas 10 Compras Pagas:");
+        for (Compra compra : ultimasComprasPagas) {
+            System.out.println(compra);
+        }
+    }
+}
+
+private void compraMaisCara() {
+    Compra compraMaisCara = null;
+    double maiorValor = 0;
+
+    for (Compra compra : compras) {
+        if (compra.getValorTotal() > maiorValor) {
+            maiorValor = compra.getValorTotal();
+            compraMaisCara = compra;
+        }
+    }
+
+    if (compraMaisCara != null) {
+        System.out.println("Compra Mais Cara:");
+        System.out.println(compraMaisCara);
+    } else {
+        System.out.println("Histórico não encontrado.");
+    }
+}
+
+private void compraMaisBarata() {
+    Compra compraMaisBarata = null;
+    double menorValor = Double.MAX_VALUE;
+
+    for (Compra compra : compras) {
+        if (compra.getValorTotal() < menorValor) {
+            menorValor = compra.getValorTotal();
+            compraMaisBarata = compra;
+        }
+    }
+
+    if (compraMaisBarata != null) {
+        System.out.println("Compra Mais Barata:");
+        System.out.println(compraMaisBarata);
+    } else {
+        System.out.println("Histórico não encontrado.");
+    }
+}
+
+private void valorTotalComprasUltimos12Meses() {
+    LocalDate dataAtual = LocalDate.now();
+    double[] totalComprasPorMes = new double[12];
+
+    for (Compra compra : compras) {
+        LocalDate dataCompra = compra.getData();
+        Period periodo = Period.between(dataCompra, dataAtual);
+        int mesesAtras = periodo.getYears() * 12 + periodo.getMonths();
+
+        if (mesesAtras < 12) {
+            totalComprasPorMes[11 - mesesAtras] += compra.getValorTotal();
+        }
+    }
+
+    for (int i = 0; i < totalComprasPorMes.length; i++) {
+        System.out.println("Mês " + (i + 1) + ": R$" + totalComprasPorMes[i]);
+    }
+
+    if (compras.isEmpty()) {
+        System.out.println("Histórico não encontrado.");
+    }
+}
 }
 
